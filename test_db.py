390b10f5d0e3a6ca1c522a3ee46248b1df97c4c8
Priyank Chaurasiya <priyankchaurasiya539@gmail.com>
2026-06-11 18:32:41 +0530

    Update the test_db file

--- test_db.py
+++ test_db.py
@@ -1,19 +1,20 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 try:
-    # 1. Connection object taiyar karo
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="157565",
+        password=os.getenv("DB_PASSWORD"),
         database="bnpl_db"
     )
     
-    # 2. Check karo connection successful hua ya nahi
     if mydb.is_connected():
-        print("🎉 Connection Successful! Python and MySQL are now linked.")
+        print(" Connection Successful! Python and MySQL are now linked.")
         
     mydb.close()
 
 except Exception as e:
-    print("❌ Error while connecting to MySQL:", e)
+    print("Error while connecting to MySQL:", e)
